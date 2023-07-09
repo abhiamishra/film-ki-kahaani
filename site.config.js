d7cfc39a13633fbaf97d7017bb246527d626bad2
abhiamishra <abhiamishra0@gmail.com>
2023-07-09 17:49:32 -0500

    fix

--- site.config.js
+++ site.config.js
@@ -18,13 +18,13 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "film-ki-kahaan",
+    title: "film-ki-kahaani",
     description: "welcome to Film Ki Kahaani!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://film-ki-kahaani-79wl.vercel.app/",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
